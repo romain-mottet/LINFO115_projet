8bfeb8ec99c42b065b1b3d181226eaf9297ecbdc
manu-vaillant-afk <manu.vaillant@student.hers.be>
2022-04-15 13:21:30 +0200

    wip

--- adj.py
+++ adj.py
@@ -1,10 +1,27 @@
 from json.tool import main
 from csv import DictReader
 import pandas as pd
+import sys
 
 
-def create_adj_matrix():
-    adj = [[]]
+class Node:
+    def __init__(self, source, target, weight, timestamp):
+        self.source = source
+        self.target = target
+        self.weight = weight
+        self.timestamp = timestamp
+
+
+class Graph:
+    def __init__(self, adj=[[]], number_vertices=0, number_edges=0, nodes=[]):
+        self.adj = adj
+        self.number_vertices = number_vertices
+        self.number_edges = number_edges
+        self.nodes = nodes
+
+
+def create_graph():
+    g = Graph()
     with open('Project dataset.csv', 'r') as read_obj:
         csv_dict_reader = DictReader(read_obj)
         # Get number of vertices to create adjency matrix
@@ -15,40 +32,45 @@ def create_adj_matrix():
         max_target_value = target_column.max()
         max_vertices = max_source_value if max_source_value > max_target_value else max_target_value
         adj = [[] for x in range(max_vertices)]
+        g.number_vertices = int(max_vertices)
 
+        # Get number of edges
         rows = list(csv_dict_reader)
-        total_rows = len(rows)
+        g.number_edges = len(rows)
         for row in rows:
             s = int(row['Source'])
             t = int(row['Target'])
             adj[s].append(t)
-    return adj
+
+        g.adj = adj
+    return g
 
 
-def count_number_components(adj: [[]]):
+def count_number_components(g: Graph):
+    marked = [False] * g.number_vertices
+    cpt = 0
+
+    for vertice in range(g.number_vertices):
+        if not marked[vertice]:
+            depth_first_search(g, vertice, marked)
+            cpt+=1
+    return cpt
+
+
+def depth_first_search(g: Graph, v:int, marked):
+    marked[v] = True
+    for w in g.adj[v]:
+        if not marked[w]:
+            depth_first_search(g, w, marked)
+
+
+def count_number_bridges(adj):
     pass
 
-
-def count_number_bridges(adj: [[]]):
-    pass
-
-# def panda_create_adj_matrix():
-#     df = pd.read_csv ('Project dataset.csv')
-#     source_column = df["Source"]
-#     max_source_value = source_column.max()
-#     target_column = df["Target"]
-#     max_target_value = target_column.max()
-#     max = max_source_value if max_source_value > max_target_value else max_target_value
-#     print(max)
-            
-    
 if __name__ == '__main__':
-    adj = create_adj_matrix()
-    print(adj[0])
-    print(adj[1])
-    print(adj[2])
-    print(adj[3])
-    print(adj[4])
-    print(adj[5])
-    print(adj[6])
-    print(len(adj))
+    sys.setrecursionlimit(2000)
+    g = create_graph()
+    cpt = count_number_components(g)
+    print(cpt)
+    # list = [False] * 10
+    # print(list)
